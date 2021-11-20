70f3b5f43849a5a9e2ad431b9a8b5d2157346288
Vladimir Caterov <vlad.caterov@mail.utoronto.ca>
2021-11-20 11:05:55 -0500

    demo.txt as best as I can get it for now

--- cleaning_data.py
+++ cleaning_data.py
@@ -1,31 +1,50 @@
+from os import name
 from typing import final
 import pandas as pd
+from pandas.core.dtypes.missing import notnull
 ########################################################
 # CLEANING WARDS
-ward_df = pd.read_csv('originals\City-Wards-Data.csv')
-names = pd.concat([ward_df['AREA_NAME']])
+# ward_df = pd.read_csv('originals\City-Wards-Data.csv')
+# names = ward_df['AREA_NAME']
 
+# final_df = names
+# df1 = pd.DataFrame({'name':name,'year': 2019} for name in names)
+# df2 = pd.DataFrame({'name':name,'year': 2020} for name in names)
+# df3 = pd.DataFrame({'name':name,'year': 2021} for name in names)
 
-final_df = pd.DataFrame(columns=list('AB'))
-years = [2019, 2020, 2021]
+# final_df = pd.concat([df1, df2, df3], ignore_index=True)
 
-# final_df = pd.concat([pd.DataFrame([name, 2019])for name in names], [pd.DataFrame([name, 2020])for name in names], [pd.DataFrame([name, 2021])for name in names],
-#                      ignore_index=True)
-for year in years:
-    for name in names:
-        final_df = final_df.append([name, year])
-
-
-final_df.to_csv('ward-data.csv',
-                index=False, header=False)
-
-print(names)
+# final_df.to_csv('ward-data.csv',
+#                 index=False, header=False)
 
 # CLEANING EMPLOYMENT RATE
 empRate_df = pd.read_csv("originals\Employment Rate (Toronto Residents).csv")
-for row in empRate_df:
-    for col in row:
-        print(col)
+january_df = empRate_df['January']
+february_df = empRate_df['February']
+march_df = empRate_df['March']
+april_df = empRate_df['April']
+may_df = empRate_df['May']
+june_df = empRate_df['June']
+july_df = empRate_df['July']
+august_df = empRate_df['August']
+sept_df = empRate_df['September']
+october_df = empRate_df['October']
+november_df = empRate_df['November']
+december_df = empRate_df['December']
+
+
+# print(january_df)
+# print(empRate_df.head)
+final_df = pd.DataFrame()
+monthly_dfs = []
+for col in empRate_df.iloc[:, [1,2,3,4,5,6,7,8,9,10,11,12]]:
+    for row in col:
+        if empRate_df.at[2, 'June'] == 'nan':
+            empRate_df.at[2, 'June'] == 'NULL'
+        final_df = pd.concat([pd.DataFrame({'month': empRate_df.at[2, [col]]})])
+# print(empRate_df)      
+
+final_df.csv('employment-rate-data.csv', index=False, header=False)
 # ward_df = pd.read_csv('CSC343Project\originals\City-Wards-Data.csv')
 # names = ward_df['AREA_NAME']
 # names.to_csv('CSC343Project\cleaned\ward-data.csv', index=False, header=False)
--- schema.ddl
+++ schema.ddl
@@ -16,7 +16,6 @@ SET SEARCH_PATH TO JobMarket;
 
 -- );
 
--- CHANGED: To Be Reviewed
 CREATE TABLE Companies (
     companyID INTEGER NOT NULL,
     companyName TEXT NOT NULL,
@@ -24,43 +23,26 @@ CREATE TABLE Companies (
     numOfEmployees INTEGER NOT NULL,
 
     PRIMARY KEY (companyID)
-    FOREIGN KEY (wardName) REFERENCES Wards(wardName),
+    FOREIGN KEY (wardName) REFERENCES NewEstablishments(wardName),
 );
 
 
 CREATE TABLE JobSectors (
     sectorName TEXT NOT NULL,
     subSector TEXT NOT NULL,
-    -- growthRate FLOAT NOT NULL,
 
     PRIMARY KEY (sectorName, subSector)
 );
 
-
--- CREATE TABLE Wards (
---     wardName TEXT NOT NULL,
---     PRIMARY KEY (wardName)
--- );
-
--- NEW TABLE FOR NEW ESTABLISHMENTS
-CREATE TABLE Wards (
+CREATE TABLE NewEstablishments (
     wardName TEXT NOT NULL,
     year INTEGER NOT NULL,
-    newEstablishments INTEGER NOT NULL,
+    newEstablishments INTEGER NOT NULL DEFAULT 0,
 
     PRIMARY KEY (wardName, year),
     CONSTRAINT validyear
         CHECK (year = 2019 OR year = 2020 OR year = 2021)
 );
-
-
--- No longer have to worry about closed companies
--- CREATE TABLE ClosedCompanies (
---     companyName TEXT NOT NULL,
---     wardName TEXT NOT NULL,
---     closureDate TEXT NOT NULL
--- );
-
 
 CREATE TABLE CityOfTorontoStatistics (
     year INTEGER NOT NULL,
@@ -74,17 +56,7 @@ CREATE TABLE CityOfTorontoStatistics (
     numOfBusinessLicencesRenewed INTEGER NOT NULL,
     numOfNewBusinessLicencesIssued INTEGER NOT NULL,
 
-    PRIMARY KEY (d, numOfEmployedCityOfTorontoResidents, percentageOfSelfEmployedTorontoResidents,
-    percentageOfVacantOfficeSpace, numOfEmploymentInsuranceBeneficiaries, numOfBusinessLicencesRenewed,
-    numOfNewBusinessLicencesIssued)
+    PRIMARY KEY (year, month)
 );
 
-
-
--- DROP table if exists Jobs cascade;
--- DROP table if exists Companies cascade;
--- DROP table if exists JobSectors cascade;
--- DROP table if exists Wards cascade;
--- DROP table if exists ClosedCompanies cascade;
--- DROP table if exists CityOfTorontoStatistics cascade;
 
